0b2061d257e57bf40a0f3393c3022989c0169a6e
Waleed794 <53418492+Wali794@users.noreply.github.com>
2023-03-13 01:07:15 -0400

    working FrontEnd

---
+++ GMLQueryRewriter/KG_Meta.py
@@ -0,0 +1,57 @@
+from rdflib import Graph
+import rdflib
+import gzip
+from rdflib.plugins.sparql.parser import parseQuery
+from rdflib.plugins.sparql import parser
+import os
+# g = Graph()
+# fpath = r'D:/CoDs Lab/kgnet/KGNET_MetaGraph.ttl'
+# print(rdflib.util.guess_format(fpath, fmap=None))
+# g.parse(fpath,format=rdflib.util.guess_format(fpath, fmap=None))
+
+query_str = """ 
+PREFIX kgnet: <http://www.kgnet.ai/>
+PREFIX dblp:  <https://dblp.org/rdf/schema/>
+
+SELECT ?apiUrl
+WHERE {
+  ?nodeClassifier a <kgnet:types/NodeClassifier> ;
+                  <kgnet:GML/NodeLabel> <dblp:venue> ;
+                  <kgnet:GML/TargetNode> <dblp:publication> ;
+  <kgnet:term/uses> ?gmlModel .
+  ?gmlModel <kgnet:API_URL> ?apiUrl .
+
+}
+
+"""
+
+query_v2 = """
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX kgnet: <https://www.kgnet.ai/>
+
+    #SELECT ?NodeClassifier
+    SELECT ?apiUrl
+    WHERE
+    {
+        ?NodeClassifier <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <kgnet:types/NodeClassifier> .
+        ?NodeClassifier <kgnet:GML/TargetNode> <dblp:Publication> .
+        ?NodeClassifier <kgnet:GML/NodeLabel> <dblp:venue> .
+        ?NodeClassifier
+        <kgnet:term/uses> ?gmlModel .
+        ?gmlModel <kgnet:API_URL> ?apiUrl .
+        }
+"""
+
+def kgnet_getModelURI(query,PATH_TO_KG = os.path.join('.','GMLQueryRewriter','KGNET_MetaGraph.ttl')):
+    g = Graph()
+    g.parse(PATH_TO_KG,format=rdflib.util.guess_format(PATH_TO_KG, fmap=None))
+    results = g.query(query)
+    for row in results:
+        return (row[0])
+# Parse the query string into a Query object
+
+# Run the query and iterate over the results
+# results = g.query(query_v2)
+# for row in results:
+#     # api_url = row['api_url']
+#     print(row[0])
---
+++ GMLWebServiceApis/Flask_GML.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 20:57:00 2023
+
+@author: walee
+"""
+from flask import Flask, request,render_template,redirect,url_for
+import GML_to_CSV as gcsv
+import GMLQueryRewriter.gmlRewriter as qr
+import time
+import os
+# import sparqlEndpoints.sparqlEndpoint as se
+
+test_query = """ 
+prefix dblp:<https://dblp.org/rdf/schema#>
+prefix kgnet: <https://www.kgnet.ai/>
+select ?title ?venue 
+where {
+?paper a dblp:Publication.
+?paper dblp:title ?title.
+?paper <https://dblp.org/rdf/schema#publishedIn> ?o.
+
+
+?paper ?NodeClassifier ?venue.
+
+?NodeClassifier a <kgnet:types/NodeClassifier>.
+?NodeClassifier <kgnet:GML/TargetNode> <dblp:paper>.
+?NodeClassifier <kgnet:GML/NodeLabel> <dblp:venue>.}
+limit 10
+  """
+# print(qr.execute(test_query))
+
+# data_query = qr.execute(test_query)[0]
+# gcsv.sparqlToCSV(data_query, 'temp/temp.csv')
+# TEMP_FILE = 'flask_temp.csv'
+
+def get_query_results(query,filename):
+    
+    data_query = qr.execute(query)[0]
+    gcsv.sparqlToCSV(data_query, filename)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html',)
+
+@app.route('/post_query',methods=['POST'])
+def post_query():
+    query = request.form['query']
+    filename = os.path.join('temp',time.strftime("%Y%m%d-%H%M%S"+'.csv'))
+    get_query_results(query,filename)
+    while not os.path.exists(filename):
+        time.sleep(0.5)
+    return redirect(url_for('display_results',filename=filename))
+    # return 'Query Executed Successfully !'+query
+
+@app.route('/display_results',methods=['GET'])
+def display_results():
+    filename = request.args.get('filename','')
+    html_table = gcsv.csvToHTML(filename)
+    return render_template('table.html', html_table = html_table)
+
+
+if __name__=="__main__":
+    app.run()
