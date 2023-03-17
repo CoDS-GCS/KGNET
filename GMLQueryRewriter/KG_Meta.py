a8eaff852720dbb83f836d53cbcf2a0b53afa444
Waleed794 <waleedafandi.794@gmail.com>
2023-03-17 14:28:31 -0400

    NodeClassification

--- GMLQueryRewriter/KG_Meta.py
+++ GMLQueryRewriter/KG_Meta.py
@@ -1,46 +1,39 @@
 from rdflib import Graph
 import rdflib
-import gzip
-from rdflib.plugins.sparql.parser import parseQuery
-from rdflib.plugins.sparql import parser
 import os
-# g = Graph()
-# fpath = r'D:/CoDs Lab/kgnet/KGNET_MetaGraph.ttl'
-# print(rdflib.util.guess_format(fpath, fmap=None))
-# g.parse(fpath,format=rdflib.util.guess_format(fpath, fmap=None))
 
-query_str = """ 
-PREFIX kgnet: <http://www.kgnet.ai/>
-PREFIX dblp:  <https://dblp.org/rdf/schema/>
+# query_str = """ 
+# PREFIX kgnet: <http://www.kgnet.ai/>
+# PREFIX dblp:  <https://dblp.org/rdf/schema/>
 
-SELECT ?apiUrl
-WHERE {
-  ?nodeClassifier a <kgnet:types/NodeClassifier> ;
-                  <kgnet:GML/NodeLabel> <dblp:venue> ;
-                  <kgnet:GML/TargetNode> <dblp:publication> ;
-  <kgnet:term/uses> ?gmlModel .
-  ?gmlModel <kgnet:API_URL> ?apiUrl .
+# SELECT ?apiUrl
+# WHERE {
+#   ?nodeClassifier a <kgnet:types/NodeClassifier> ;
+#                   <kgnet:GML/NodeLabel> <dblp:venue> ;
+#                   <kgnet:GML/TargetNode> <dblp:publication> ;
+#   <kgnet:term/uses> ?gmlModel .
+#   ?gmlModel <kgnet:API_URL> ?apiUrl .
 
-}
+# }
 
-"""
+# """
 
-query_v2 = """
-PREFIX dblp: <https://dblp.org/rdf/schema#>
-PREFIX kgnet: <https://www.kgnet.ai/>
+# query_v2 = """
+# PREFIX dblp: <https://dblp.org/rdf/schema#>
+# PREFIX kgnet: <https://www.kgnet.ai/>
 
-    #SELECT ?NodeClassifier
-    SELECT ?apiUrl
-    WHERE
-    {
-        ?NodeClassifier <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <kgnet:types/NodeClassifier> .
-        ?NodeClassifier <kgnet:GML/TargetNode> <dblp:Publication> .
-        ?NodeClassifier <kgnet:GML/NodeLabel> <dblp:venue> .
-        ?NodeClassifier
-        <kgnet:term/uses> ?gmlModel .
-        ?gmlModel <kgnet:API_URL> ?apiUrl .
-        }
-"""
+#     #SELECT ?NodeClassifier
+#     SELECT ?apiUrl
+#     WHERE
+#     {
+#         ?NodeClassifier <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <kgnet:types/NodeClassifier> .
+#         ?NodeClassifier <kgnet:GML/TargetNode> <dblp:Publication> .
+#         ?NodeClassifier <kgnet:GML/NodeLabel> <dblp:venue> .
+#         ?NodeClassifier
+#         <kgnet:term/uses> ?gmlModel .
+#         ?gmlModel <kgnet:API_URL> ?apiUrl .
+#         }
+# """
 
 def kgnet_getModelURI(query,PATH_TO_KG = os.path.join('.','GMLQueryRewriter','KGNET_MetaGraph.ttl')):
     g = Graph()
