7522c6ac949cae9365fe85781ee509c4cd65e5bf
afandi <waleedafandi.794@gmail.com>
2024-03-12 22:07:17 -0400

    Added full batch kgwise inference with file based target nodes

--- GMLWebServiceApis/Inference_API_v2.py
+++ GMLWebServiceApis/Inference_API_v2.py
@@ -10,10 +10,12 @@ sys.path.append(inference_dir)
 from Constants import *
 from GMLaaS.Inference_pipeline_v2 import wise_inference
 from GMLaaS.Inference_pipeline import perform_inference
-from fastapi import FastAPI
+from fastapi import FastAPI, File,UploadFile,HTTPException,Depends,Form
 from uvicorn import run
 from typing import List
 from pydantic import BaseModel
+import json
+from fastapi.responses import JSONResponse
 import traceback
 
 app = FastAPI()
@@ -21,18 +23,78 @@ app = FastAPI()
 HOST = '0.0.0.0'
 PORT = KGNET_Config.GML_Inference_PORT
 
-class InferenceRequest(BaseModel):
-    model_id : str
-    named_graph_uri : str
-    sparqlEndpointURL : str
-    RDFEngine : str
-    dataQuery : List[str]=None
-    targetNodesQuery : str=None
-    targetNodesList:List[str] =None
-    TOSG_Pattern:str=TOSG_Patterns.d1h1
-    topk: int = 1
-    #target_rel : str
+# class InferenceRequest(BaseModel):
+#     model_id : str
+#     named_graph_uri : str
+#     sparqlEndpointURL : str
+#     RDFEngine : str
+#     dataQuery : List[str]=None
+#     targetNodesQuery : str=None
+#     targetNodesList:List[str] =None
+#     TOSG_Pattern:str=TOSG_Patterns.d1h1
+#     topk: int = 1
 
+# class InferenceRequest(BaseModel):
+#     model_id : str
+#     named_graph_uri : str
+#     sparqlEndpointURL : str
+#     RDFEngine : str
+#     dataQuery : List[str]=None
+#     targetNodesQuery : str=None
+#     targetNodesList:List[str] =None
+#     TOSG_Pattern:str=TOSG_Patterns.d1h1
+#     topk: int = 1
+
+class InferenceRequest(BaseModel):
+    model_id: str
+    named_graph_uri: str
+    sparqlEndpointURL: str = 'http://206.12.98.118:8890/sparql'
+    RDFEngine: str = 'OpenlinkVirtuoso'
+    dataQuery: List[str] = None
+    targetNodesQuery: str = None
+    targetNodesList: List[str] = None
+    TOSG_Pattern: str = TOSG_Patterns.d1h1
+    topk: int = 1
+
+    @classmethod
+    def as_form(cls,model_id: str = Form(...),named_graph_uri: str = Form(...),sparqlEndpointURL: str = Form(...),
+                RDFEngine: str = Form(...),dataQuery: List[str] = Form(None),targetNodesQuery: str = Form(None),
+                targetNodesList: List[str] = Form(None),TOSG_Pattern: str = Form(TOSG_Patterns.d1h1),
+                topk: int = Form(1)) :
+        return cls(model_id=model_id,named_graph_uri=named_graph_uri,sparqlEndpointURL=sparqlEndpointURL,RDFEngine=RDFEngine,
+                   dataQuery=dataQuery,targetNodesQuery=targetNodesQuery,targetNodesList=targetNodesList,TOSG_Pattern=TOSG_Pattern,
+                   topk=topk)
+
+
+@app.post("/fullbatch_wise/mid/{mid}")
+async def run_fullbatch_inference(mid:str,
+                              inference_request:InferenceRequest = Depends(),
+                              file:UploadFile = File(...)
+                              ):
+
+    model_id = inference_request.model_id if inference_request.model_id is not None else mid
+    named_graph_uri = inference_request.named_graph_uri
+    sparqlEndpointURL = inference_request.sparqlEndpointURL
+    RDFEngine = inference_request.RDFEngine
+    dataQuery = inference_request.dataQuery
+    targetNodesQuery = inference_request.targetNodesQuery
+    targetNodesList=inference_request.targetNodesList
+    topk = inference_request.topk
+    TOSG_Pattern=inference_request.TOSG_Pattern
+    #print(file.filename)
+    targetNodesList = []
+    file_targets = file.file.read()
+    targets = file_targets.decode().split('\n')
+    for target in targets:
+        if target.strip():
+            targetNodesList.append(target.strip())
+
+
+    dic_results = wise_inference(model_id = model_id, named_graph_uri = named_graph_uri,#target_rel=target_rel,
+                                    dataQuery= dataQuery, sparqlEndpointURL = sparqlEndpointURL,
+                                    targetNodesQuery = targetNodesQuery,
+                                    topk = topk,RDFEngine=RDFEngine,targetNodesList=targetNodesList,TOSG_Pattern=TOSG_Pattern)
+    return dic_results#['y_pred']
 
 @app.post("/wise_inference/mid/{mid}")
 async def run_wise_inference(mid:str,inference_request: InferenceRequest):
